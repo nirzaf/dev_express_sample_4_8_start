8b8770b34c18aa5a83705d3781a17405c6a5b23a
nirzaf <mfmfazrin1986@gmail.com>
2022-05-11 21:29:56 +0530

    Add project files.

---
+++ sample_4_8/PivotOverview.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.XtraEditors;
+
+namespace sample_4_8 {
+  public partial class PivotOverview : XtraUserControl {
+    public PivotOverview() {
+      InitializeComponent();
+    }
+  }
+}
